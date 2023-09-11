d3526edefb36b785f25ed4047737d4953cf851c9
t-hyland <thyland@terpmail.umd.edu>
2023-09-11 19:10:34 -0400

    SERVER STUFF

--- server/server.js
+++ server/server.js
@@ -5,17 +5,57 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+let email = "", password = "";
+
 //gives json object to the webpage 
-app.get("/message", (req, res) => {
-  res.json({ message: "Hello from server!",
+app.get("/message", (request, response) => {
+  response.json({ message: "Hello from server!",
              title: "Boobs"});
 });
 
-app.put("/boobs", (req, res) => {
-    res.json()
+app.post("/loginData", (request, response) => {
+  let user = {
+    email: request.body.email,
+    password: request.body.password,
+  }
+  insert(user);
+
+  response.send("message recieved");
 });
+
+app.get("/verification", (request, response) => {
+  if (email == "a") {
+    response.json({ message: true });
+  } else {
+    response.json({ message: false });
+  }
+})
 
 //starts the server
 app.listen(8000, () => {
   console.log(`Server is running on port 8000.`);
 });
+
+
+
+//database stuff
+const {MongoClient} = require('mongodb');
+
+async function insert(user){
+    //saves database URI
+    const uri = "mongodb+srv://t-hyland:Tomh@cluster0.0uz4cny.mongodb.net/?retryWrites=true&w=majority";
+
+    //creates client that is linked to the database
+    const client = new MongoClient(uri);
+
+    try {
+        //connects client to the database
+        await client.connect();
+
+        await client.db("RateMyProffessor").collection("posts").insertOne(user);
+    } catch (e){
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+};
